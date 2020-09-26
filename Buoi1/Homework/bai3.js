195584314a3016fd5588e7623dce549526e08ea4
codeinlife10 <codeinlife10@gmail.com>
2020-09-26 11:03:37 +0700

    buoi1

--- Buoi1/Homework/bai3.js
+++ Buoi1/Homework/bai3.js
@@ -1,4 +1,4 @@
-let array = [1,1,2,3,4]
+
 
 
 //Cach1 
@@ -25,22 +25,37 @@ let array = [1,1,2,3,4]
 
 //Bai tap sau khi thay đổi đề :))
 
-let countTime = {}
+// let countTime = {}
 
-for(let i = 0 ; i < array.length ; i++){
-    if(!(array[i] in countTime)){
-        countTime[array[i]] = 1
-    }else{
-        countTime[array[i]]++
-    }  
-}
+// for(let i = 0 ; i < array.length ; i++){
+//     if(!(array[i] in countTime)){
+//         countTime[array[i]] = 1
+//     }else{
+//         countTime[array[i]]++
+//     }  
+// }
 
+// let newArray = []
+// for(let i = 0 ; i < array.length ; i++){
+//     if(countTime[array[i]] == 1){
+//         newArray.push(array[i])
+//     }
+// }
+
+// console.log(...newArray)
+let array = [1,1,2,3,4]
 let newArray = []
 for(let i = 0 ; i < array.length ; i++){
-    if(countTime[array[i]] == 1){
-        newArray.push(array[i])
+    let count = 0
+    for(let j = 0 ; j < array.length ; j++){
+      if(i != j){
+          if(array[i] == array[j]){
+                count++
+          }
+      }
+}
+    if(count == 0){
+      newArray.push(array[i])
     }
 }
-
-console.log(...newArray)
-
+console.log(...newArray);
